73c452b54cff46dcb15cb9f4d29006e264f7ac2f
Amol Nandivale <nandivaleamol@gmail.com>
2022-12-17 09:58:24 +0530

    spring-boot 3.0.0 spring security

--- src/main/java/com/springboot/security/services/impl/UserServiceImpl.java
+++ src/main/java/com/springboot/security/services/impl/UserServiceImpl.java
@@ -11,10 +11,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
